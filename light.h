5f3ef6fa11431ea22e3e7f2d389813021bae418d
maribalke <99878745+maribalke@users.noreply.github.com>
2022-02-17 12:13:09 +0100

    Add files via upload

---
+++ light.h
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "driver/elevio.h"
+
+void clear_light(); //slukker lys
+void floor_light(int floor); //etasjelys
+void stop_lamp(void); //stopplys
